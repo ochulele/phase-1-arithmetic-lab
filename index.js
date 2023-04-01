55e6331d36fe184a39ac2ddb3e8fbbf1782e5ae9
ochuk <ochuciano@gmail.com>
2023-04-01 05:13:13 -0400

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num3 = 22;
+let num4 = 6;
+const mod = num3 % num4;
 
-// Write your code here
+const max = Math.max(3, 5, 12, 15, 20);
+
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+Math.isInteger(random);
